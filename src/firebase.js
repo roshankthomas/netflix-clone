3bf280e39851fb5539c5f560c48040b000a995a8
Roshan Koshy Thomas <thomasr13@uni.coventry.ac.uk>
2023-03-24 16:29:33 +0000

    Netflix clone initialised

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB44m8DUrjtOvdnLsIVjMuFM3SL4Y1RZ6I",
+  authDomain: "netflix-clone-build-218cd.firebaseapp.com",
+  projectId: "netflix-clone-build-218cd",
+  storageBucket: "netflix-clone-build-218cd.appspot.com",
+  messagingSenderId: "460789224448",
+  appId: "1:460789224448:web:0ac9667a4bf33858fdf4e5"
+};
